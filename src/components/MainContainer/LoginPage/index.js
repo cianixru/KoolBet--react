ff71afe6f4bfcd7c48f5af860832f7eb88bd7dce
e.safronov@pbt.com.mt <ghbdtnedrew>
2018-07-13 19:34:28 +0300

    update

--- src/components/MainContainer/LoginPage/index.js
+++ src/components/MainContainer/LoginPage/index.js
@@ -1,67 +1,63 @@
 import React from 'react';
 // import { connect } from "react-redux";
 
-export default class LoginPage extends React.Component {
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
-    render() {
-        const { classes } = this.props;
+import { form } from './form.css';
+import { input } from './input.css';
+import { button } from './button.css';
+import { checkbox } from './checkbox.css';
+
+const FormItem = Form.Item;
+
+
+class LoginPage extends React.Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+          }
+        });
+      }
+      render() {
+        const { getFieldDecorator } = this.props.form;
         return (
-            <div>
-                <div className="registration__wrapper">
-                    <div className="registration-form">
-                        <h3>
-                            Sign in to your account
-                        </h3>
-
-                        <ul>
-                            <li>
-                                <div className="input-container">
-                                    <div className="inputSection">
-                                        <input className="" type="text" placeholder="Username" />
-                                    </div>
-                                    <div className="inputSection">
-                                        <input className="" type="password" placeholder="Password" />
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="grid-container--nowrap checkbox__blk space-between">
-                                    <div className="a">
-                                        <input id="acceptTerms" type="checkbox" />
-                                        <label for="acceptTerms">Remember me</label>
-                                    </div>
-
-                                    <div className="a nw f-password">
-                                        <a href="#">
-                                            Forgot Password?
-                                            </a>
-                                    </div>
-                                </div>
-                            </li>
-
-                            <li className="submit-area">
-                                <input type="button" value="LOGIN" id="SubmitForm" className="open-account-button" />
-                            </li>
-                            <li className="social-registration">
-                                <div className="horizontal-separator-container short-separator">
-                                    <span className="horizontal-separator">or</span>
-                                </div>
-                                <div id="SubmitForm" className="open-account-button-facebook"><i className="facebook__icon"></i>Login with Facebook</div>
-                            </li>
-                        </ul>
-
-                    </div>
-                </div>
-            </div>
-
-        )
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <FormItem>
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: 'Please input your username!' }],
+              })(
+                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Please input your Password!' }],
+              })(
+                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(
+                <Checkbox>Remember me</Checkbox>
+              )}
+              <a className="login-form-forgot" href="">Forgot password</a>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+              Or <a href="">register now!</a>
+            </FormItem>
+          </Form>
+        );
+      }
     }
-}
+    const WrappedNormalLoginForm = Form.create()(LoginPage);
 
-
-
-
-
+    export default WrappedNormalLoginForm;
 
 
 // function mapStateToProps (state) {
@@ -72,3 +68,6 @@ export default class LoginPage extends React.Component {
 //   }
 
 // export default connect(mapStateToProps)(MainContainer)
+
+
+
--- src/components/MainContainer/MatchContents/index.js
+++ src/components/MainContainer/MatchContents/index.js
@@ -7,14 +7,9 @@ import RightSidebar from './RightSidebar/index';
 import MainContent from './MainContent/index';
 
 class MatchContents extends Component {
-    state = {
-        mobileToggle: true,
-    }
     render() {
         return (
             <div className="page-grid">
-
-            {/* {( this.state.mobileToggle ) ? '1' : '2' } */}
 
             <LeftSidebar />
 
